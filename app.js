adf7244d389d2f70ef3e42ed9b6c8477738e363d
JB Wilson <53917177+Selenestica@users.noreply.github.com>
2020-01-24 22:42:51 -0600

    finally got the styles to work again

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
 const express = require('express')
 const app = express()
-const path = require('path')
-const mustacheExpress = require('mustache-express')
 const bodyParser = require('body-parser')
-const indexRouter = require('./routes/index')
-var session = require('express-session')
-
+const path = require('path')
 const VIEWS_PATH = path.join(__dirname, '/views')
+const mustacheExpress = require('mustache-express')
+
+const session = require('express-session')
+
 app.use(bodyParser.urlencoded({extended: false}))
 
-app.use('/', indexRouter)
+app.get('/', (req, res) => {
+  res.render('index')
+})
 
+app.use(express.static(path.join(__dirname, 'partials')))
+app.use(express.static(path.join(__dirname, '/public')))
 app.engine('mustache', mustacheExpress(VIEWS_PATH + '/partials', '.mustache'))
-app.set('views', VIEWS_PATH)
+app.set('views', './views')
 app.set('view engine', 'mustache')
 
 app.listen(2800, () => {
-  console.log("Server started on Port 3000, at " + Date())
+  console.log("Server started on Port 2800, at " + Date())
 })
