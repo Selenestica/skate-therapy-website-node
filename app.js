db25806cf9569878947b7e31d8f92b0f647eb6b2
JB Wilson <53917177+Selenestica@users.noreply.github.com>
2020-01-26 16:13:42 -0600

    major changes including routes, carousel, and layout

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//Variable Set-Up
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
@@ -5,14 +6,55 @@ const path = require('path')
 const VIEWS_PATH = path.join(__dirname, '/views')
 const mustacheExpress = require('mustache-express')
 
+//========== express-session ========
 const session = require('express-session')
 
 app.use(bodyParser.urlencoded({extended: false}))
 
+//Routes
+const donateRouter = require('./routes/donate')
+app.use('/donate', donateRouter)
+
+const volunteerRouter = require('./routes/volunteer')
+app.use('/volunteer', volunteerRouter)
+
+const enrollRouter = require('./routes/enroll')
+app.use('/enroll', enrollRouter)
+
+const sponsorRouter = require('./routes/sponsor')
+app.use('sponsor', sponsorRouter)
+
+const galleryRouter = require('./routes/gallery')
+app.use('gallery', galleryRouter)
+
+//Home
 app.get('/', (req, res) => {
   res.render('index')
 })
 
+//Donate Page
+app.get('/donate', (req, res) => {
+  res.render('donate')
+})
+
+app.get('/volunteer', (req, res) => {
+  res.render('volunteer')
+})
+
+app.get('/enroll', (req, res) => {
+  res.render('enroll')
+})
+
+app.get('/sponsor', (req, res) => {
+  res.render('sponsor')
+})
+
+app.get('/gallery', (req, res) => {
+  res.render('gallery')
+})
+
+
+//Mustache
 app.use(express.static(path.join(__dirname, 'partials')))
 app.use(express.static(path.join(__dirname, '/public')))
 app.engine('mustache', mustacheExpress(VIEWS_PATH + '/partials', '.mustache'))
